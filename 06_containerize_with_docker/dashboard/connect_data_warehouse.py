179867bc763185258464912dc7e22038a715e9ef
Milou Sanden <Milou.Sanden@gmail.com>
2025-10-06 15:38:47 +0200

    updated connect dashboard

--- 06_containerize_with_docker/dashboard/connect_data_warehouse.py
+++ 06_containerize_with_docker/dashboard/connect_data_warehouse.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import duckdb
+import os 
 
-# data warehouse directory
-db_path = str(Path(__file__).parents[1] / "data_warehouse/job_ads.duckdb")
+DB_PATH = os.getenv("DUCKDB_PATH")
  
 def query_job_listings(query='SELECT * FROM marts.mart_technical_jobs'):
-    with duckdb.connect(db_path, read_only=True) as conn:
+    with duckdb.connect(DB_PATH, read_only=True) as conn:
         return conn.query(f"{query}").df()
 
